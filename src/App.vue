83704458cc6ff188f969d24236cbc9e9e6d9c9c7
John Bogart <john.bogart@bbc.co.uk>
2020-02-07 15:18:34 +0000

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Swansdom</h1>
+    <Quote />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Quote from './components/Quote.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Quote
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
